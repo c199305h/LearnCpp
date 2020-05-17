fd275f4b69d6db066c24ff15c40e589d53723369
achan <344238796@qq.com>
2020-05-17 18:11:55 +0800

    g++ -c -o learn

---
+++ src/temp/test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+void fn();
+int main()
+{
+  std::cout<<"test1.cpp"<<std::endl;
+  fn();
+  return 0;
+}
---
+++ src/temp/test2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void fn(){
+    std::cout<<"test2.cpp"<<std::endl;
+}
