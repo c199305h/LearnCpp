e15934c26feb1a2f4c16b2ba9cb789c2617f5f97
achan <344238796@qq.com>
2020-05-17 18:28:24 +0800

    learn 1.2

---
+++ src/Unit1/pra1_3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// pratice 1.3
+int main()
+{
+    std::cout << "hello,world" << std::endl;
+}
---
+++ src/Unit1/pra1_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// pratice 1.4
+int main()
+{
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " multi " << v2 << " is " << v1 * v2 << std::endl;
+    return 0;
+}
